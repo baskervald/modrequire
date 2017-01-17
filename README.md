d8d717dcdef4c423c77c528577a08c5c5c0703e5
Ashlynn <ashlynn.wonderland@gmail.com>
2017-01-17 00:42:50 -0500

    Basic functionality working, at least.

---
+++ README.md
@@ -0,0 +1,3 @@
+Mod Require
+---
+Reimplements (compiled) require in The Binding of Isaac Afterbirth+ mods
---
+++ modrequire.py
@@ -0,0 +1,25 @@
+from sys import argv
+import re
+import os.path
+from os import getcwd
+
+pattern = re.compile('((?:require(?:\(|\ )(?:\"|\'))(.+)(?:\"|\')(?:\))?)')
+basedir = os.path.dirname(os.path.realpath(argv[1]))
+
+def parsefile(path, outfile):
+    with open(path, 'r') as infile:
+        for line in infile:
+            match = pattern.search(line)
+            if match is None:
+                outfile.write(line + "\n")
+            else:
+                split = line.split(match.group(1))
+                outfile.write(split[0] + '(function() \n')
+                splitreq = match.group(2).split('.')
+                splitreq[-1] = splitreq[-1] + '.lua'
+                parsefile(os.path.join(basedir, *splitreq), outfile)
+                outfile.write('end)()\n'+split[1]+'\n')
+
+
+with open(argv[2], 'w+') as outfile:
+    parsefile(argv[1], outfile)
