fc4f8a096aa762c4c64610b3e1dda999507ad463
Ashlynn <ashlynn.wonderland@gmail.com>
2017-01-17 00:57:46 -0500

    Removed \ns

--- modrequire.py
+++ modrequire.py
@@ -11,14 +11,14 @@ def parsefile(path, outfile):
         for line in infile:
             match = pattern.search(line)
             if match is None:
-                outfile.write(line + "\n")
+                outfile.write(line)
             else:
                 split = line.split(match.group(1))
-                outfile.write(split[0] + '(function() \n')
+                outfile.write(split[0] + '(function()\n')
                 splitreq = match.group(2).split('.')
                 splitreq[-1] = splitreq[-1] + '.lua'
                 parsefile(os.path.join(basedir, *splitreq), outfile)
-                outfile.write('end)()\n'+split[1]+'\n')
+                outfile.write('\nend)()')
 
 
 with open(argv[2], 'w+') as outfile:
