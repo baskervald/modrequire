e49d3e5c15d9289ce28cddf386d39ea55897f57d
Ashlynn <ashlynn.wonderland@gmail.com>
2017-01-17 19:19:13 -0500

    V2

--- modrequire.py
+++ modrequire.py
@@ -1,25 +1,35 @@
 from sys import argv
 import re
 import os.path
-from os import getcwd
 
-pattern = re.compile('((?:require(?:\(|\ )(?:\"|\'))(.+)(?:\"|\')(?:\))?)')
+pattern = re.compile('(?:require(?:\(|\ )(?:\"|\'))(.+)(?:\"|\')(?:\))?')
 basedir = os.path.dirname(os.path.realpath(argv[1]))
+requires = {}
 
 def parsefile(path, outfile):
     with open(path, 'r') as infile:
-        for line in infile:
-            match = pattern.search(line)
-            if match is None:
-                outfile.write(line)
-            else:
-                split = line.split(match.group(1))
-                outfile.write(split[0] + '(function()\n')
-                splitreq = match.group(2).split('.')
-                splitreq[-1] = splitreq[-1] + '.lua'
-                parsefile(os.path.join(basedir, *splitreq), outfile)
-                outfile.write('\nend)() ' + split[1])
+        text = infile.read()
+        matches = pattern.findall(text)
+        return (text, matches)
+
+def handlematches(matches, outfile):
+    for match in matches:
+        if not match in requires:
+            path = match.split('.')
+            path[-1] = path[-1] + '.lua'
+            text, reqs = parsefile(os.path.join(basedir, *path), outfile)
+            requires[match] = text
+            handlematches(reqs, outfile)
+
+def createfunctions(reqs, outfile):
+    outfile.write('__modrequire_functions={}\n')
+    for req in reqs:
+        outfile.write('__modrequire_functions["' + req + '"]=function()\n' + reqs[req] + '\nend\n')
 
 
 with open(argv[2], 'w+') as outfile:
-    parsefile(argv[1], outfile)
+    text, matches = parsefile(argv[1], outfile)
+    handlematches(matches, outfile)
+    createfunctions(requires, outfile)
+    outfile.write('__modrequire_modules={} function require(req) if __modrequire_modules[req] == nil then __modrequire_modules[req] = __modrequire_functions[req]() end return __modrequire_modules[req] end\n')
+    outfile.write(text)
