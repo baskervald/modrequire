86aadddff8a7590a59e6feb5b4c93b6c8ccecbbe
Ashlynn <ashlynn.wonderland@gmail.com>
2017-01-24 19:46:33 -0500

    Make variables local

--- modrequire.py
+++ modrequire.py
@@ -22,7 +22,7 @@ def handlematches(matches, outfile):
             handlematches(reqs, outfile)
 
 def createfunctions(reqs, outfile):
-    outfile.write('__modrequire_functions={}\n')
+    outfile.write('local __modrequire_functions={}\n')
     for req in reqs:
         outfile.write('__modrequire_functions["' + req + '"]=function()\n' + reqs[req] + '\nend\n')
 
@@ -31,5 +31,5 @@ with open(argv[2], 'w+') as outfile:
     text, matches = parsefile(argv[1], outfile)
     handlematches(matches, outfile)
     createfunctions(requires, outfile)
-    outfile.write('__modrequire_modules={} function require(req) if __modrequire_modules[req] == nil then __modrequire_modules[req] = __modrequire_functions[req]() end return __modrequire_modules[req] end\n')
+    outfile.write('local __modrequire_modules={} function require(req) if __modrequire_modules[req] == nil then __modrequire_modules[req] = __modrequire_functions[req]() end return __modrequire_modules[req] end\n')
     outfile.write(text)
